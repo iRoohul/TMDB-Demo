f8ef4572212935b4d4a914fdf2efe809625deb36
Roohul Amin Khan <roohulk@mindfiresolutions.com>
2021-05-16 14:56:49 +0530

    Initial project set up

---
+++ TMDB-Demo/Modules/Movie Details/MovieDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDetailsViewController.swift
+//  TMDB-Demo
+//
+//  Created by roohulk on 16/05/21.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
